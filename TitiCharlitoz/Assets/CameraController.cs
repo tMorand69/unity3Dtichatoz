858cdad267db46b7efcca471a693f82c14d27ade
Thibaud Demoranddejouffrey <thibaud.demoranddejouffrey@supinfo.com>
2021-03-05 17:56:07 +0100

    push

---
+++ TitiCharlitoz/Assets/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //public GameObject player;
+    private Vector3 offset;
+    public float turnSpeed = 4.0f;
+    public Transform player;
+    public float xaxis = -1.85f;
+    public float yaxis = 0.77f;
+    public float zaxis = -5.52f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offset = new Vector3(player.position.x + xaxis, player.position.y + yaxis, player.position.z + zaxis);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //offset = new Vector3(player.position.x + xaxis, player.position.y + yaxis, player.position.z + zaxis);
+
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        //transform.position = player.position + offset;
+        //transform.LookAt(player.position);
+    }
+}
---
+++ TitiCharlitoz/Assets/CharacterAiming.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public float turnSpeed = 15;
+    public float aimDuration = 0.15f;
+
+    Camera mainCamera;
+    RaycastWeapon weapon;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        weapon = GetComponentInChildren<RaycastWeapon>();
+    }
+
+    void FixedUpdate()
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,yawCamera,0),turnSpeed*Time.fixedDeltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        if (weapon)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                weapon.StartFiring();
+            }
+            if (weapon.isFiring)
+            {
+                weapon.UpdateFiring(Time.deltaTime);
+            }
+            weapon.UpdateBullets(Time.deltaTime);
+            if (Input.GetButtonUp("Fire1"))
+            {
+                weapon.StopFiring();
+            }
+        }
+
+    }
+}
