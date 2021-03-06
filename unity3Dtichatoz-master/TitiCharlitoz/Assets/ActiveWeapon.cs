677a1618f62fdf1d2749d1953ba03463bb76b057
emieuxcharles <emieux.charles@gmail.com>
2021-03-06 12:22:53 +0100

    firstcommit

---
+++ unity3Dtichatoz-master/TitiCharlitoz/Assets/ActiveWeapon.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    public Transform crossHairTarget;
+    public UnityEngine.Animations.Rigging.Rig handIk;
+    public Transform weaponParent;
+    public Transform weaponLeftGrip;
+    public Transform weaponRightGrip;
+
+    RaycastWeapon weapon;
+    public Animator rigController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaycastWeapon existingWeapon = GetComponentInChildren<RaycastWeapon>();
+        if (existingWeapon)
+        {
+            Equip(existingWeapon);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (weapon)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                weapon.StartFiring();
+            }
+            if (weapon.isFiring)
+            {
+                weapon.UpdateFiring(Time.deltaTime);
+            }
+            weapon.UpdateBullets(Time.deltaTime);
+            if (Input.GetButtonUp("Fire1"))
+            {
+                weapon.StopFiring();
+            }
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                bool isHolstered = rigController.GetBool("holster_weapon");
+                rigController.SetBool("holster_weapon", !isHolstered);
+            }
+            
+        }
+    }
+
+    public void Equip(RaycastWeapon newWeapon)
+    {
+        if (weapon)
+        {
+            Destroy(weapon.gameObject);
+        }
+        weapon = newWeapon;
+        weapon.raycastDestination = crossHairTarget;
+        weapon.transform.parent = weaponParent;
+        weapon.transform.localPosition = Vector3.zero;
+        weapon.transform.localRotation = Quaternion.identity;
+        rigController.Play("equip_" + weapon.weaponName);
+    }
+}
